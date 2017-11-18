ed037018f1bd18146fd5b227c2d062538044bafe
derrekchow <derrekdc@gmail.com>
2017-11-18 05:21:51 -0500

    servo 5

--- servo/main.py
+++ servo/main.py
@@ -1,34 +1,39 @@
 import RPi.GPIO as GPIO
 import PIL
 import picamera
-import io
+import os
 from time import sleep
+import socket
+port = 5000
 
 BTN = 18
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-stream = io.BytesIO()
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 640)
-    camera.start_recording(stream, format='h264', quality=23)
-    camera.wait_recording(15)
-    camera.stop_recording()
+def socket():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(("", port))
+	print "waiting on port:", port
+	while 1:
+	    data, addr = s.recvfrom(1024)
+	    print data
+
 
 class Servo():
 	def __init__(self, PIN):
 		GPIO.setup(PIN, GPIO.OUT)
 		self.pwm = GPIO.PWM(PIN, 50)
 		self.pwm.start(7.5)
-		self.angle = 0
 
 	def setAngle(self, angle):
 		set_angle = 0.053*angle + 2.2
 		self.pwm.ChangeDutyCycle(set_angle)
-		self.angle = set_angle
 
 	def move(self, angle):
 		self.setAngle(0)
-		sleep(1)
 		self.setAngle(120)
+
 
 def move(n):
 	if(n == 1):
@@ -41,23 +46,6 @@ def move(n):
 		servoO.move()
 
 
-def setup():
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	servoY = Servo(17)
-	servoB = Servo(27)
-	servoG = Servo(22)
-	servoO = Servo(23)
-	
-
-def destroy():
-	servoY.stop()
-	servoB.stop()
-	servoG.stop()
-	servoO.stop()
-	GPIO.cleanup()
-
 def loop():
 	flag = True
 	print("program running")
@@ -65,12 +53,20 @@ def loop():
 		btnState = GPIO.input(BTN)
 		if btnState == 0:
 			print("button press")
-			move(1)	
+			move(1)
 
 
 if __name__ == '__main__':		# Program start from here
 	try:
-		setup()
+		os.system('./raspi.sh')
+		servoY = Servo(17)
+		servoB = Servo(27)
+		servoG = Servo(22)
+		servoO = Servo(23)
 		loop()
 	except KeyboardInterrupt:	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
+		servoY.stop()
+		servoB.stop()
+		servoG.stop()
+		servoO.stop()
+		GPIO.cleanup()
