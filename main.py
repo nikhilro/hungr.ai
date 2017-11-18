7bbee44f41e41ce88143fa95bd8370993a350fd0
derrekchow <derrekdc@gmail.com>
2017-11-17 22:22:55 -0500

    intial servo

---
+++ main.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+class Servo():
+	def __init__(self, PIN):
+		self.PIN = PIN
+		GPIO.setup(self.PIN, GPIO.OUT)
+		self.pwm = GPIO.PWM(self.PIN, 50)
+		self.pwm.start(7.5)
+		self.angle = 0
+
+	def setAngle(self, angle):
+		self.angle = 0.053*angle + 2.2
+		self.pwm.ChangeDutyCycle(self.angle)
+
+	def adjustAngle(self, angle):
+		self.setAngle(self.angle + angle)
+
+	def stop(self):
+		self.pwm.stop()
+
+
+def loop():
+	while True:
+		new_angle = input("set servo angle: ")
+		servo1.setAngle(new_angle)
+		
+
+
+if __name__ == '__main__':		# Program start from here
+	try:
+		servo1 = Servo(17)
+		loop()
+	except KeyboardInterrupt:	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		servo1.stop()
+		GPIO.cleanup()
