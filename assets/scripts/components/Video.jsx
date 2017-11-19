eefb0f41ad61b59562a683a525675c4be5656ca5
raphael <raph.koh@gmail.com>
2017-11-19 09:43:41 -0500

    changes

--- assets/scripts/components/Video.jsx
+++ assets/scripts/components/Video.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8000');
+// const socket = openSocket('http://localhost:8000');
 
 
 export default class Video extends Component {
@@ -17,20 +17,20 @@ export default class Video extends Component {
       'data': ""
     }
 
-    socket.on('connect', function() {
-        console.log('Socket connected!');
-        socket.send('read_camera');
-    });
-
-    socket.on('videoStart', function(messageEvent) {
-      const video = document.querySelector('#video');
-      let enc = new TextDecoder("utf-8");
-      const data = enc.decode(messageEvent.data);
-
-      this.setState({
-        'data': "data:image/jpeg;base64," + data
-      });
-    });
+    // socket.on('connect', function() {
+    //     console.log('Socket connected!');
+    //     socket.send('read_camera');
+    // });
+    //
+    // socket.on('videoStart', function(messageEvent) {
+    //   const video = document.querySelector('#video');
+    //   let enc = new TextDecoder("utf-8");
+    //   const data = enc.decode(messageEvent.data);
+    //
+    //   this.setState({
+    //     'data': "data:image/jpeg;base64," + data
+    //   });
+    // });
   }
 
   render() {
